ff9bce3516d14063dd442dc6aa9573fd7e5fa6f2
cmptr077 <37179901+cmptr077@users.noreply.github.com>
2018-09-25 12:14:52 +0900

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+# from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+        #    redirect_url = request.GET.get('next', 'home')
+        #    return redirect(redirect_url)
+            return render(request, 'home.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('home')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
