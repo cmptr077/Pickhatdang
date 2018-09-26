f7c12c8493923df9dbcddb588085dc7d1fbd3d7b
cmptr077 <37179901+cmptr077@users.noreply.github.com>
2018-09-27 04:13:01 +0900

    Update urls.py

--- logintest/urls.py
+++ logintest/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from . import views
 
 
-urlpatterns = {
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('accounts/', include('accounts.urls', namespace="accounts"))
-}
+]
